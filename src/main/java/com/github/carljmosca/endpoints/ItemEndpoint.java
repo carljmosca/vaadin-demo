35ef365d539213019ad14dd690b43b411d2989f9
Carl J. Mosca <carljmosca@gmail.com>
2021-02-12 23:00:49 -0500

    added ip address

--- src/main/java/com/github/carljmosca/endpoints/ItemEndpoint.java
+++ src/main/java/com/github/carljmosca/endpoints/ItemEndpoint.java
@@ -6,10 +6,18 @@
 package com.github.carljmosca.endpoints;
 
 import com.github.carljmosca.pojo.Item;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
 import com.vaadin.flow.server.connect.Endpoint;
 import com.vaadin.flow.server.connect.auth.AnonymousAllowed;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +27,7 @@ import java.util.ArrayList;
 public class ItemEndpoint {
 
     List<Item> result = new ArrayList<>();
-    
+
     public ItemEndpoint() {
         populateList();
     }
@@ -34,7 +42,7 @@ public class ItemEndpoint {
             String likes, String comments, String shares) {
         Item item = new Item();
         item.setImg(img);
-        item.setName(name);
+        item.setName(name + "(" + getHostIp() + ")");
         item.setDate(date);
         item.setPost(posts);
         item.setLikes(likes);
@@ -72,34 +80,42 @@ public class ItemEndpoint {
 
         addItem(
                 "https://randomuser.me/api/portraits/women/24.jpg",
-                 "Emmy Elsner",
-                 "Apr 22",
-                 "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                 "1K",
-                 "500",
-                 "20"
+                "Emmy Elsner",
+                "Apr 22",
+                "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
+                "1K",
+                "500",
+                "20"
         );
         addItem(
                 "https://randomuser.me/api/portraits/men/76.jpg",
-                 "Alf Huncoot",
-                 "Apr 21",
-                 "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                 "1K",
-                 "500",
-                 "20"
+                "Alf Huncoot",
+                "Apr 21",
+                "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
+                "1K",
+                "500",
+                "20"
         );
 
         addItem(
                 "https://randomuser.me/api/portraits/women/76.jpg",
-                 "Lidmila Vilensky",
-                 "Apr 17",
-                 "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                 "1K",
-                 "500",
-                 "20"
+                "Lidmila Vilensky",
+                "Apr 17",
+                "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
+                "1K",
+                "500",
+                "20"
         );
 
     }
+
+    private String getHostIp() {
+        String ip = "";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ItemEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ip;
+    }
 }
-
-
